4d078c42724d8bc3a093f229adf8be1d72881d75
Zmicier Kruts <zmicier@Zmiciers-MacBook-Pro.local>
2025-05-16 15:18:00 +0200

    variable type errors in gws-au

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_internet_gateway" "main" {
     vpc_id = "${var.vpc_id}"
 
 
-    tags {
+    tags = {
         Name = "igw-${var.region_code}-${var.env}"
         region = "${var.region_code}"
         env = "${var.env}"
